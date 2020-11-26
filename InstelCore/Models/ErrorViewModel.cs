f729cff0a042ba3b294297409b7420321403a3b2
MATIN-VAIO <MATIN-VAIO@MATIN-VAIO>
2020-11-26 19:34:37 +0330

    Update

---
+++ InstelCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InstelCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
